8d79223cc5fb8b37d759cd96a0e238b84c680a5a
coder_h <harsh.sinha@students.iiit.ac.in>
2017-11-18 01:36:22 +0530

    add

--- html/web.js
+++ html/web.js
@@ -1,26 +1,13 @@
-function rule() {
-    var x = document.createElement("IMG");
-    x.setAttribute("src", "rule1.png");
-    x.setAttribute("id", "rule");
-    document.body.appendChild(x);
-    document.write("   ");
-}
-function map() { 
-var myCenter = new google.maps.LatLng(17.447891, 78.348589);
-function initialize(){
-    var mapProp = {
-        center:myCenter,
-        zoom:12,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
+function initMap() {
+        var uluru = {lat: 17.448019759651796, lng:78.34837943565503 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 18,
+          center: uluru
 
-    var map = new google.maps.Map(document.getElementById("map"),mapProp);
-
-    var marker = new google.maps.Marker({
-        position:myCenter,
-    });
-
-    marker.setMap(map);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-}
+        });
+        var marker = new google.maps.Marker({
+          position :uluru,
+          map: map
+        });
+      }
+  
